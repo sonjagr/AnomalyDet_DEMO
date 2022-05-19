dd100a7af548087717671cfe1ece32550c553577
sgroenro <sonja.gronroos@cern.ch>
2022-05-19 14:16:56 +0200

    clening

--- process_train_CNN_tfdata_brightness_test.py
+++ process_train_CNN_tfdata_brightness_test.py
@@ -9,7 +9,7 @@ from old_codes.autoencoders import *
 import matplotlib.pyplot as plt
 import random, time, argparse
 import tensorflow as tf
-from helpers.cnn_helpers import crop, patch_images,patch_labels, rotate_image, ds_length, grad, tf_bright_image, tf_bayer2rgb, tf_rgb2bayer, bright_image
+from helpers.cnn_helpers import ds_length, grad, preprocess
 
 tf.keras.backend.clear_session()
 
@@ -43,75 +43,8 @@ cont_epoch = args.contfromepoch
 
 os.environ["CUDA_VISIBLE_DEVICES"] = gpu
 
-## autoencode and return difference
-@tf.function
-def encode(img):
-    img = tf.reshape(img, [-1, 2720, 3840, INPUT_DIM])
-    encoded_img = ae.encode(img)
-    decoded_img = ae.decode(encoded_img)
-    aed_img = tf.sqrt(tf.pow(tf.subtract(img, decoded_img), 2))
-    return aed_img
-
 random.seed(42)
 bce = tf.keras.losses.BinaryCrossentropy(from_logits=False)
-
-def preprocess(imgs, lbls, to_encode = True, normal_times = 50, to_augment = False, to_brightness = False, to_brightness_before_ae = False, batch_size = 256):
-
-    cropped_imgs = imgs.map(crop)
-
-    if to_brightness_before_ae:
-        bright_cropped_imgs = cropped_imgs.map(tf_bayer2rgb)
-        bright_dataset_changed = bright_cropped_imgs.map(bright_image)
-        bright_cropped_images = bright_dataset_changed.map(tf_rgb2bayer)
-
-    if to_encode == True:
-        diff_imgs = cropped_imgs.map(encode)
-        if to_brightness_before_ae:
-            bright_diff_imgs = bright_cropped_imgs.map(encode)
-    if to_encode == False:
-        diff_imgs = cropped_imgs
-
-    patched_imgs = diff_imgs.flat_map(patch_images)
-    patched_lbls = lbls.flat_map(patch_labels)
-
-    patched_imgs = patched_imgs.unbatch()
-    patched_lbls = patched_lbls.unbatch()
-
-    if to_brightness_before_ae:
-        bright_patched_imgs = bright_diff_imgs.flat_map(patch_images)
-        bright_patched_imgs = bright_patched_imgs.unbatch()
-
-    dataset = tf.data.Dataset.zip((patched_imgs, patched_lbls))
-
-    anomalous_dataset = dataset.filter(lambda x, y: y == 1.)
-    anomalous_nbr_before = ds_length(anomalous_dataset)
-    anomalous_images = anomalous_dataset.map(lambda x, y: x)
-    anomalous_labels = anomalous_dataset.map(lambda x, y: y)
-
-    if to_brightness_before_ae:
-        bright_dataset = tf.data.Dataset.zip((bright_patched_imgs, patched_lbls))
-        bright_anomalous_dataset = bright_dataset.filter(lambda x, y: y == 1.)
-        bright_anomalous_images = bright_anomalous_dataset.map(lambda x, y: x)
-        bright_anomalous_labels = bright_anomalous_dataset.map(lambda x, y: y)
-
-    if to_augment == True:
-        rotated_dataset = anomalous_images.map(lambda x: rotate_image(x))
-        rotated_anomalous_dataset = tf.data.Dataset.zip((rotated_dataset, anomalous_labels))
-        combined_anomalous_dataset = anomalous_dataset.concatenate(rotated_anomalous_dataset)
-        if to_brightness == True:
-            bright_dataset_rgb = anomalous_images.map(tf_bayer2rgb)
-            bright_dataset_changed = bright_dataset_rgb.map(bright_image)
-            bright_dataset_bayer = bright_dataset_changed.map(tf_rgb2bayer)
-            bright_anomalous_dataset = tf.data.Dataset.zip((bright_dataset_bayer, anomalous_labels))
-            bright_anomalous_dataset = bright_anomalous_dataset.map(lambda x, y: (tf.cast(x, tf.float32), y))
-            combined_anomalous_dataset = combined_anomalous_dataset.concatenate(bright_anomalous_dataset)
-    if to_augment == False:
-        combined_anomalous_dataset = anomalous_dataset
-    anomalous_nbr = ds_length(combined_anomalous_dataset)
-    normal_dataset = dataset.filter(lambda x, y: y == 0.).shuffle(500).take(normal_times * anomalous_nbr)
-
-    combined_dataset_batch = normal_dataset.concatenate(combined_anomalous_dataset).shuffle(buffer_size =  20000, reshuffle_each_iteration = True).batch(batch_size=batch_size, drop_remainder=True)
-    return combined_dataset_batch, anomalous_nbr_before, anomalous_nbr
 
 ae = AutoEncoder()
 print('Loading autoencoder and data...')
@@ -120,47 +53,38 @@ base_dir = '/afs/cern.ch/user/s/sgroenro/anomaly_detection/db/'
 dir_det = 'DET/'
 images_dir_loc = '/data/HGC_Si_scratch_detection_data/MeasurementCampaigns/'
 X_train_det_list = np.load(base_dir + dir_det + 'X_train_DET.npy', allow_pickle=True)
-X_test_det_list = np.load(base_dir + dir_det + 'X_test_DET.npy', allow_pickle=True)
+X_val_det_list = np.load(base_dir + dir_det + 'X_test_DET.npy', allow_pickle=True)
 X_train_det_list = [images_dir_loc + s for s in X_train_det_list]
-X_test_det_list = [images_dir_loc + s for s in X_test_det_list]
+X_val_det_list = [images_dir_loc + s for s in X_val_det_list]
 Y_train_det_list = np.load(base_dir + dir_det + 'Y_train_DET.npy', allow_pickle=True).tolist()
-Y_test_det_list = np.load(base_dir + dir_det + 'Y_test_DET.npy', allow_pickle=True).tolist()
+Y_val_det_list = np.load(base_dir + dir_det + 'Y_test_DET.npy', allow_pickle=True).tolist()
 
+N_det_val = int(len(X_val_det_list))
 ## how many samples used
-X_train_det_list = X_train_det_list[:200]
-Y_train_det_list = Y_train_det_list[:200]
-X_test_det_list = X_test_det_list[:100]
-Y_test_det_list = Y_test_det_list[:100]
+X_train_det_list = X_train_det_list
+Y_train_det_list = Y_train_det_list
+X_val_det_list = X_val_det_list[:int(N_det_val/2)]
+Y_val_det_list = Y_val_det_list[:int(N_det_val/2)]
 
-N_det_test = int(len(X_test_det_list))
 N_det_train = len(X_train_det_list)
-print('Loaded number of train, test samples: ', N_det_train, N_det_test)
+print('Loaded number of train, val samples: ', N_det_train, N_det_val)
 print('All loaded. Starting processing...')
 time1 = time.time()
 
 train_imgs = create_dataset(X_train_det_list)
 train_lbls = tf.data.Dataset.from_tensor_slices(Y_train_det_list)
 
-test_imgs = create_dataset(X_test_det_list)
-test_lbls = tf.data.Dataset.from_tensor_slices(Y_test_det_list)
+val_imgs = create_dataset(X_val_det_list)
+val_lbls = tf.data.Dataset.from_tensor_slices(Y_val_det_list)
 
-train_combined_dataset_batch, train_ano_before, train_ano = preprocess(train_imgs, train_lbls, to_encode = True, normal_times = 50, to_augment = True, to_brightness = True, to_brightness_before_ae=False, batch_size = batch_size)
-val_combined_dataset_batch, val_ano_before, val_ano = preprocess(test_imgs, test_lbls, to_encode = True, normal_times = 50, to_augment = False, batch_size = batch_size)
+train_combined_dataset_batch, train_ano_before, train_ano = preprocess(train_imgs, train_lbls, to_encode = True, ae = ae, normal_times = 10, to_augment = True, to_brightness = False, to_brightness_before_ae=False, batch_size = batch_size,  drop_rem = True)
+val_combined_dataset_batch, val_ano_before, val_ano = preprocess(val_imgs, val_lbls, to_encode = True, ae = ae, normal_times = 10, to_augment = False, batch_size = batch_size, drop_rem = False)
 val_combined_dataset = val_combined_dataset_batch.unbatch()
 
 train_dataset_len = ds_length(train_combined_dataset_batch.unbatch())
 val_dataset_len = ds_length(val_combined_dataset)
 print('Number of training, validation patches in total: ', train_dataset_len, val_dataset_len)
 print('Number of anomalous training, validation patches before and after augment: ', train_ano_before, val_ano_before, train_ano, val_ano)
-
-### PLOTTING FOR CROSSCHECK
-w = 0
-for x, y, in train_combined_dataset_batch.unbatch():
-    if y == 1 and w < 10:
-        plt.imshow(tf.reshape(x, [160, 160]))
-        plt.show()
-        w = w+1
-####
 
 time2 = time.time()
 pro_time = time2-time1
@@ -179,7 +103,10 @@ else:
     train_loss_results, val_loss_results = [], []
     train_accuracy_results, val_accuracy_results = [], []
     from CNNs import *
-    model = model_tf
+    if model_ID == 'model_tf':
+        model = model_tf
+    if model_ID == 'model_simple':
+        model = model_simple
 
 print(model.summary())
 
@@ -197,7 +124,6 @@ for epoch in tqdm(range(cont_epoch, num_epochs), total = num_epochs):
         epoch_train_loss_mean.append(loss_value)
         y_hat =  model(x, training=True)
         #epoch_train_accuracy.update_state(y_true = y, y_pred = y_hat)
-    print(y_hat)
     train_loss_results.append(np.mean(epoch_train_loss_mean))
     #train_accuracy_results.append(epoch_train_accuracy.result().numpy())
 
@@ -210,7 +136,7 @@ for epoch in tqdm(range(cont_epoch, num_epochs), total = num_epochs):
         y_hat = tf.cast(logits, tf.uint64)
         #epoch_val_accuracy.update_state(y_true = val_y, y_pred = y_hat)
 
-    #print("Test set accuracy: {:.3%}".format(epoch_val_accuracy.result()))
+    #print("val set accuracy: {:.3%}".format(epoch_val_accuracy.result()))
 
     val_loss_results.append(np.mean(epoch_val_loss_mean))
     #val_accuracy_results.append(epoch_val_accuracy.result().numpy())
@@ -226,7 +152,7 @@ for epoch in tqdm(range(cont_epoch, num_epochs), total = num_epochs):
         print('Model checkpoint saved to ', model_saveto)
         np.save('saved_CNNs/%s/cnn_%s_val_loss.npy' % (savename, savename), val_loss_results)
         np.save('saved_CNNs/%s/cnn_%s_train_loss.npy' % (savename, savename), train_loss_results)
-        print('Model test and train losses saved to ', 'saved_CNNs/%s/' % savename)
+        print('Model val and train losses saved to ', 'saved_CNNs/%s/' % savename)
 
         plt.plot(np.arange(0, len(val_loss_results)), val_loss_results, label = 'Val loss')
         plt.plot(np.arange(0, len(train_loss_results)), train_loss_results, label ='Train loss')
