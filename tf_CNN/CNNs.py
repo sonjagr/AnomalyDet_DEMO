4fc979a39613f65cb33f61a162d884127992cce4
sgroenro <sonja.gronroos@cern.ch>
2022-05-25 17:38:31 +0200

    update

--- tf_CNN/CNNs.py
+++ tf_CNN/CNNs.py
@@ -45,10 +45,9 @@ model_tf2 = tf.keras.Sequential([
 model_simple = tf.keras.Sequential([
     tf.keras.layers.InputLayer(input_shape=(160,160, 1)),
     tf.keras.layers.experimental.preprocessing.Rescaling(1. / 243.),
-    #tf.keras.layers.RandomBrightness(factor = 0.2, value_range=(0, 243.), seed=None),
-    tf.keras.layers.Conv2D(filters=64, kernel_size=(40,40), strides=(40,40), padding="valid",activation='relu'),
+    tf.keras.layers.Conv2D(filters=8, kernel_size=(20,20), strides=(20,20), padding="valid",activation='relu'),
     tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Conv2D(filters=64, kernel_size=(4, 4), strides=(4, 4),  padding="valid", activation='relu'),
+    tf.keras.layers.Conv2D(filters=16, kernel_size=(8, 8), strides=(8, 8),  padding="valid", activation='relu'),
     tf.keras.layers.Dropout(0.2),
     tf.keras.layers.Conv2D(filters=1, kernel_size=(1,1), strides=(1,1), activation = 'sigmoid', padding="valid"),
     tf.keras.layers.Flatten(),
@@ -57,7 +56,7 @@ model_simple = tf.keras.Sequential([
 model_simple2 = tf.keras.Sequential([
     tf.keras.layers.InputLayer(input_shape=(160,160, 1)),
     tf.keras.layers.experimental.preprocessing.Rescaling(1. / 243.),
-    tf.keras.layers.Conv2D(filters=8, kernel_size=(20,20), strides=(20,20), padding="valid",activation='relu'),
+    tf.keras.layers.Conv2D(filters=4, kernel_size=(10,10), strides=(10,10), padding="valid",activation='relu'),
     tf.keras.layers.Dropout(0.2),
     tf.keras.layers.Conv2D(filters=8, kernel_size=(8, 8), strides=(8, 8),  padding="valid", activation='relu'),
     tf.keras.layers.Dropout(0.2),
